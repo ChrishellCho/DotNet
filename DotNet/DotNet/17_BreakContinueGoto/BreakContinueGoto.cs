26560be9af91ae1f38edd0e8c7ab24b307f01dc8
YeJin Cho <83330070+gkdldu@users.noreply.github.com>
2021-06-17 17:59:37 +0900

    17: Continue 구문

--- DotNet/DotNet/17_BreakContinueGoto/BreakContinueGoto.cs
+++ DotNet/DotNet/17_BreakContinueGoto/BreakContinueGoto.cs
@@ -78,6 +78,8 @@ namespace DotNet._17_BreakContinueGoto
 
 		static void ContinueDemo()
 		{
+			// continue문 : 반복문에서 아래 실행문을 실행하지 않고 다음 구문(조건절)으로 이동
+
 			//[!] 1~100까지 정수 중 3의 배수를 제외한 수의 합
 			int sum = 0;
 			for (int i = 1; i <= 100; i++)
